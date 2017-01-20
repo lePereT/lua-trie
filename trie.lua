374986ede700e3d71f0e0e965650aa9a4a81cfbf
Vladislav Yarmak <vladislav@vm-0.com>
2017-01-21 01:45:49 +0200

    retabulate source file

--- trie.lua
+++ trie.lua
@@ -1,44 +1,44 @@
 local methods = {}
 
 local function mknode() 
-	return {
-		children = {},
-		value = nil
-	}
+    return {
+        children = {},
+        value = nil
+    }
 end
 
 function methods:set(keyiter, value)
-	local cur_node = self.root
-	for x in keyiter do
-		local child = cur_node.children[x]
-		if child == nil then
-			child = mknode()
-			cur_node.children[x] = child
-		end
-		cur_node = child
-	end
-	cur_node.value = value
+    local cur_node = self.root
+    for x in keyiter do
+        local child = cur_node.children[x]
+        if child == nil then
+            child = mknode()
+            cur_node.children[x] = child
+        end
+        cur_node = child
+    end
+    cur_node.value = value
 end
 
 function methods:get(keyiter)
-	local cur_node = self.root
-	for x in keyiter do
-		local child = cur_node.children[x]
-		if child == nil then
-			return nil
-		end
-		cur_node = child
-	end
-	return cur_node.value
+    local cur_node = self.root
+    for x in keyiter do
+        local child = cur_node.children[x]
+        if child == nil then
+            return nil
+        end
+        cur_node = child
+    end
+    return cur_node.value
 end
 
 function methods:longest_prefix(keyiter)
     local cur_node = self.root
-	local prefix = {}
+    local prefix = {}
     local value = nil
 
-	local stack = {}
-	for x in keyiter do
+    local stack = {}
+    for x in keyiter do
         if cur_node.value ~= nil then
             value = cur_node.value
             for _, v in ipairs(stack) do
@@ -47,28 +47,28 @@ function methods:longest_prefix(keyiter)
             stack = {}
         end
 
-		local child = cur_node.children[x]
-		if cur_node.children[x] == nil then
-			break
+        local child = cur_node.children[x]
+        if cur_node.children[x] == nil then
+            break
         end
 
         table.insert(stack, x)
         cur_node = cur_node.children[x]
-	end
+    end
     if cur_node.value ~= nil then
         value = cur_node.value
         for _, v in ipairs(stack) do
             table.insert(prefix, v)
         end
     end
-	return prefix, value
+    return prefix, value
 end
 
 local function new()
-	return setmetatable({
-		root = mknode()
-	},
-	{
+    return setmetatable({
+        root = mknode()
+    },
+    {
         __index = methods
     })
 end
